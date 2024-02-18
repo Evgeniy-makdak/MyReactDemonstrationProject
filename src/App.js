05341844013718d0f11fb56f1880a8123c68f14f
Evgeniy <58253815+Evgeniy-makdak@users.noreply.github.com>
2024-02-18 19:16:11 +0300

    Add files via upload
    Implemented sorting of posts by certain values ​​(by title and description).

--- src/App.js
+++ src/App.js
@@ -3,9 +3,8 @@ import Counter from './components/Counter';
 import Handle from './components/Handle';
 import PostList from './components/PostList';
 import './styles/App.css';
-import MyButton from './components/UI/button/MyButton';
-import MyInput from './components/UI/button/input/MyInput';
 import PostForm from './components/PostForm';
+import MySelect from './components/UI/select/MySelect';
 
 // Обратите внимание, что мы можем переиспользовать компоненты по сколько угодно раз и в разных местах программы.
 // При этом эти компоненты будут работать совершенно независимо друг от друга.
@@ -15,9 +14,9 @@ function App() {
   // массив постов и будем перебирать их с помощью функции map():
 
   const [posts, setPosts] = useState([
-    { id: 1, title: 'first note', body: 'description' },
-    { id: 2, title: 'second note', body: 'description' },
-    { id: 3, title: 'third note', body: 'description' },
+    { id: 1, title: 'first note', body: 'aaa' },
+    { id: 2, title: 'second note', body: 'sss' },
+    { id: 3, title: 'third note', body: 'ddd' },
   ]);
 
   // Можно добавить второй список постов:
@@ -28,15 +27,22 @@ function App() {
   //   { id: 3, title: 'another third note', body: 'description1' },
   // ]);
 
+  const [selectedSort, setSelectedSort] = useState('');
+
   const createPost = (newPost) => {
-    setPosts([...posts, newPost])
-  }
+    setPosts([...posts, newPost]);
+  };
 
   // Реализуем функционал удаления постов. Так как мы не можем пробрасывать пропсы от дочернего компонента к родительскому,
   // то создаём функцию обратного вызова с аргументом, вызывающим пост:
 
   const removePost = (post) => {
-    setPosts(posts.filter(p => p.id != post.id))
+    setPosts(posts.filter((p) => p.id != post.id));
+  };
+
+  const sortPosts = (sort) => {
+    setSelectedSort(sort);
+    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
   }
 
   return (
@@ -51,19 +57,23 @@ function App() {
       *************************************************
       <PostForm create={createPost} />
       {/* Чтобы при удалении всех постов было сообщение об этом, создадим "условную отрисовку, используя тернарный оператор": */}
-      <hr style={{margin: '16px 0'}}/>
+      <hr style={{ margin: '16px 0' }} />
       <div>
-        <select>
-          <option value="value1">By title</option>
-          <option value="value1">By description</option>
-        </select>
+        <MySelect
+          value={selectedSort}
+          onChange={sortPosts}
+          defaultvalue="sort by"
+          options={[
+            { value: 'title', name: 'by name' },
+            { value: 'body', name: 'by description' },
+          ]}
+        />
       </div>
-      {
-        posts.length != 0
-          ? <PostList remove={removePost} posts={posts} title="Post's list 1" />
-
-          : <h2 style={{textAlign: 'center'}}>No posts now</h2>
-      }
+      {posts.length != 0 ? (
+        <PostList remove={removePost} posts={posts} title="Post's list 1" />
+      ) : (
+        <h2 style={{ textAlign: 'center' }}>No posts now</h2>
+      )}
       {/* Второй список постов:  */}
       {/* <PostList posts1={posts1} title="Post's list 2" /> */}
     </div>
